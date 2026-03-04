1fac46724221892eba12e4c96b644d67d58b6443
VaibhavK <vaibhavkumar199649@gmail.com>
2026-03-04 12:44:04 +0530

    golang formatted

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -2,6 +2,6 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
 	fmt.Println("Welcome to Students Api Project in Go!")
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -13,33 +13,32 @@ type HTTPServer struct {
 //env-default:"production"
 
 type Config struct {
-	Env string `yaml:"env" env:"ENV" env-required:"true"`
+	Env         string `yaml:"env" env:"ENV" env-required:"true"`
 	StoragePath string `yaml:"storage_path" env-required:"true"`
-	HTTPServer `yaml:"http_server"`
+	HTTPServer  `yaml:"http_server"`
 }
 
 func MustLoad() {
-	var configPath string 
+	var configPath string
 	configPath = os.Getenv("CONFIG_PATH")
 
 	if configPath == "" {
 		// Checking if some configuration is passed on commandline
-		flags := flag.String("config","", "path to configuration file")
+		flags := flag.String("config", "", "path to configuration file")
 		flag.Parse()
 		configPath = *flags
 	}
 
-	// If still the Config Path is Empty then throw the error 
-	
+	// If still the Config Path is Empty then throw the error
+
 	if configPath == "" {
 		// We don't want the code to go through beyond this!
 		log.Fatal("Config path Not Set!")
 	}
 
 	// If the Config is Not Empty then check if the file storage path is correct or not!
-	if _, err := os.Stat(configPath); os.IsNotExist(err){
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
 		log.Fatalf("Config File Does Not Exist: %s", configPath)
 	}
 
-	
 }
